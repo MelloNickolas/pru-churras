ae328e26e65f353908f85c31d6ca276d54b003a6
MelloNickolas <nickolasphmello10@gmail.com>
2023-03-24 08:57:22 -0300

    Personalização Cara ou Coroa

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -16,28 +16,28 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  cara = 'assets/cara.png';
-  coroa = 'assets/coroa.png';
-  logo = 'assets/logo.png';
+  cara = 'https://sorteador.com.br/imgs/flip-a-coin/side-b.jpeg';
+  coroa = 'https://sorteador.com.br/imgs/flip-a-coin/side-a.jpeg';
+  logo = 'https://sorteador.com.br/imgs/flip-a-coin/side-b.jpeg';
   image = this.logo;
-  info = 'clique no botão para jogar!';
+  info = 'Clique no botão para jogar!';
   state = 'default';
 
   constructor() { }
 
   jogarMoeda() {
 
-    this.info = 'girando...';
+    this.info = 'GIRANDO...';
     this.image = this.logo;
     this.state = this.state === 'default' ? 'rotated' : 'default';
 
     setTimeout(() => {
       if (Math.random() < 0.5) {
         this.image = this.cara;
-        this.info = "cara!"
+        this.info = "CARA"
       } else {
         this.image = this.coroa;
-        this.info = "Coroa!"
+        this.info = "COROA"
       }
     }, 2000);
   }
